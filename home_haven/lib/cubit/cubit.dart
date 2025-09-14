9ee9f1264f9b95f40a075cc0113a89035e5e0778
Mona Said <mona.said162002@gmail.com>
2025-09-14 03:46:05 +0300

    building home screen

--- home_haven/lib/cubit/cubit.dart
+++ home_haven/lib/cubit/cubit.dart
@@ -5,11 +5,6 @@ import 'package:home_haven/modules/cart/cart_screen.dart';
 import 'package:home_haven/modules/home/home_screen.dart';
 import 'package:home_haven/modules/profile/profile_screen.dart';
 
-class CarousalModel {
-  String? image;
-  CarousalModel({this.image});
-}
-
 class HomeHavenCubit extends Cubit<HomeHavenStates> {
   HomeHavenCubit() : super(InitialState());
 
@@ -26,9 +21,9 @@ class HomeHavenCubit extends Cubit<HomeHavenStates> {
     'My Cart',
     'Profile',
   ];
-  List<CarousalModel> images = [
-    CarousalModel(image: 'assets/images/banner1.png'),
-    CarousalModel(image: 'assets/images/banner2.png'),
+  final List<String> images = [
+    'assets/images/banner1.png',
+    'assets/images/banner2.png',
   ];
   int currentIndex = 0;
 
--- home_haven/lib/modules/home/home_screen.dart
+++ home_haven/lib/modules/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:hexcolor/hexcolor.dart';
 import '../../cubit/cubit.dart';
 import '../../cubit/states.dart';
 
@@ -14,22 +15,58 @@ class HomeScreen extends StatelessWidget {
       builder: (BuildContext context, state) {
         var cubit = HomeHavenCubit.get(context);
         return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // CarouselSlider(
-            //     items: ,
-            //     options: CarouselOptions(
-            //       height: 200.0,
-            //       initialPage: 0,
-            //       viewportFraction: 1.0,
-            //       reverse: false,
-            //       enableInfiniteScroll: true,
-            //       autoPlay: true,
-            //       enlargeCenterPage: false,
-            //       autoPlayCurve: Curves.fastEaseInToSlowEaseOut,
-            //       autoPlayInterval: const Duration(seconds: 3),
-            //       autoPlayAnimationDuration: const Duration(seconds: 1),
-            //       scrollDirection: Axis.horizontal,
-            //     )),
+            SizedBox(
+              height: 5.0,
+            ),
+            CarouselSlider(
+                items: cubit.images.map((image) {
+                  return Builder(builder: (BuildContext context) {
+                    return Container(
+                      padding: EdgeInsetsDirectional.only(
+                        start: 5.0,
+                        end: 5.0,
+                      ),
+                      //margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Image.asset(
+                        image,
+                        //fit: BoxFit.cover,
+                      ),
+                    );
+                  });
+                }).toList(),
+                options: CarouselOptions(
+                  height: 200.0,
+                  initialPage: 0,
+                  viewportFraction: 1.0,
+                  reverse: false,
+                  enableInfiniteScroll: true,
+                  autoPlay: true,
+                  enlargeCenterPage: false,
+                  autoPlayCurve: Curves.fastEaseInToSlowEaseOut,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  autoPlayAnimationDuration: const Duration(seconds: 1),
+                  scrollDirection: Axis.horizontal,
+                )),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Special Offers',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      color: HexColor('#404040'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+            ),
           ],
         );
       },
