5b437b6bfa9339b676033d4b22fd4dee2e10867c
Mona Said <mona.said162002@gmail.com>
2025-10-10 01:49:11 +0300

    remove secret keys

--- home_haven/lib/payment_gateway_stripe/stripe_keys.dart
+++
@@ -1,6 +0,0 @@
-abstract class StripeKeys {
-  static const String publishableKey =
-      "pk_test_51SEJzIBrYIrU7Ag9yAwev8QiUBWIYh8b36OGScUTygu0iS2w8ThOGegeKEAEl3qP9OGYKHL4lJxcl54Rk1UONw2z00vzs0Z3Ce";
-  static const String secretKey =
-      "sk_test_51SEJzIBrYIrU7Ag9LRx6BdRCcPvuAyHYYFGvGGarUPEHrrbM2IrICnjs4Lz0RskTXMN86jFi7Dfh1DrikuP2kZVf00FNBf6T6o";
-}
