c72366dc8bb757baf6c2c59d6be00ef0605447b3
Mona Said <mona.said162002@gmail.com>
2025-10-09 03:03:58 +0300

    finish app

--- home_haven/lib/shared/network/remote/end_points.dart
+++ home_haven/lib/shared/network/remote/end_points.dart
@@ -1,1 +1,7 @@
 const PRODUCTS = '/api/v1/categories/3/products';
+
+// API WEBSITE LINK
+
+// https://api.escuelajs.co/api/v1/categories/3/products
+
+// https://fakeapi.platzi.com/en/rest/categories/#get-all-products-by-category
